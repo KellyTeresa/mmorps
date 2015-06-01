f62dc34aa31ea612585b75fe03497fc2f9204415
Kelly Raila <kelly.raila@gmail.com>
2015-06-01 12:29:55 -0400

    Init

---
+++ server.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "openssl"
+require 'pry'
+
+use Rack::Session::Cookie, {
+  secret: "keep_it_secret_keep_it_safe"
+}
+# binding.pry
+def generate_hmac(data, secret)
+  OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret, data)
+end
+
+
+
+get '/' do
+  erb :index
+end
